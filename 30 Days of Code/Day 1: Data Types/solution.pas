a40f5fb9ad24a37336f9f901a2b95fcad4f183e9
AnhKhoi05072006 <61281333+AnhKhoi05072006@users.noreply.github.com>
2020-09-01 18:36:33 +0700

    create solution.pas

---
+++ 30 Days of Code/Day 1: Data Types/solution.pas
@@ -0,0 +1,15 @@
+var i,i2:integer;
+    d,d2:real;
+    s,s2:string;
+begin
+    i:=4;
+    d:=4.0;
+    s:='Hackerrank ';
+    readln(i2);
+    readln(d2);
+    readln(s2);
+    writeln(i+i2);
+    writeln(d+d2:0:1);
+    writeln(s+s2);
+    readln
+end.
